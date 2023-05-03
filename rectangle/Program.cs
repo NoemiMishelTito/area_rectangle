6318cd0447906ad30b011b2b7918f512ce49475b
Mishel <mishtito19@gmail.com>
2023-05-03 11:47:35 -0400

    Add project files.

---
+++ rectangle/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Program that calculates the area of a rectangle");
+        Console.WriteLine("Set the base of a rectangle");
+        int base1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Set the height of a rectangle");
+        int height = Convert.ToInt32(Console.ReadLine());
+        int result = base1 * height;
+        Console.WriteLine("The result of the area is: " + result);
+    }
+}
